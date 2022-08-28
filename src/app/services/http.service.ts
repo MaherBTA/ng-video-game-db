8d279fa69a046ff401787b7a99a93462dd52f2a8
Maher ben <maher.ben@foton-it.com>
2022-08-28 12:03:08 +0100

    Add searchGames method in home componannt

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {environment as env} from 'src/environments/environment'
+import {environment as env} from 'src/environments/environment.prod'
 import { APIResponse, Game } from '../models';
 
 
@@ -22,7 +22,7 @@ export class HttpService {
       params = new HttpParams().set('ordering',ordering).set('search',search);
     }
 
-    return this.http.get<APIResponse<Game>>(`$(env.BASE_URL)/games`, {
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
       params:params,
     })
   }
